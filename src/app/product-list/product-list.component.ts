835f05fab50a5f931bf95c89f2537f35e6e405e4
gowtham718 <sankargowtham165@gmail.com>
2022-04-05 15:58:03 +0530

    3 files modified, 4 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,22 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+  add() {
+    window.alert('The product has been added to the cart successfully!');
+  }
+  remove() {
+    window.alert('The product has been removed from the cart successfully!');
+  }
+
+  Payment() {
+    window.alert('Your payment has made successfully!');
+  }
+  review() {
+    window.alert('Thanks for Your Review!');
+  }
+  onNotify() {
+    window.alert('Notification!');
+  }
 }
 
 
